8be402f4e56f2c26da94037a41011fad2ab44837
hotmilk0071 <spotam005@gmail.com>
2022-02-21 16:45:26 +0400

    initial

--- scripts.js
+++ scripts.js
@@ -1,4 +1,1 @@
 'use strict'
-const fs = require('fs');
-const text = fs.readFileSync('scripts.js', 'utf8');
-console.log(text);
--- server.js
+++ server.js
@@ -1,22 +1,30 @@
-'use strict';
+'use strict'
+
 const http = require('http');
 const fs = require("fs");
-
-const server = http.createServer(function (request, response){
+const server = http.createServer(function (request, response) {
     console.log(request.method, request.url);
     if (request.url === '/style.css') {
         const css = fs.readFileSync('style.css', 'utf8');
         response.end(css);
+    } else if (request.url === '/sitemap.xml'){
+        const xml = fs.readFileSync('sitemap.xml', 'utf8');
+        response.end(xml);
+    } else if (request.url === '/robots.txt'){
+        const txt = fs.readFileSync('robots.txt', 'utf8');
+        response.end(txt);
     } else {
         const html = fs.readFileSync('index.html', 'utf8');
         response.end(html);
-
     }
 });
 
+
+
+
 console.log('port = ', process.env.PORT);
 
-server.listen(process.env.PORT || 3000);
+server.listen(3000);
 
 
 console.log('Server Started');
