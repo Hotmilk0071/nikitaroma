40bec66bd40ef8133c992f67a1bc0de1426e589d
hotmilk0071 <spotam005@gmail.com>
2022-02-21 18:03:21 +0400

    initial

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const server = http.createServer(function (request, response) {
     } else if (request.url === '/sitemap.xml'){
         const xml = fs.readFileSync('sitemap.xml', 'utf8');
         response.end(xml);
+    } else if (request.url === '/test'){
+        const html = fs.readFileSync('test.html', 'utf8');
+        response.end(html);
     } else if (request.url === '/robots.txt'){
         const txt = fs.readFileSync('robots.txt', 'utf8');
         response.end(txt);
