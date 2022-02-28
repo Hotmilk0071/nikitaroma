db35fff1387abca5664bf0985e8e64e6a080bfa5
hotmilk0071 <spotam005@gmail.com>
2022-02-28 23:31:50 +0400

    initial

--- server.js
+++ server.js
@@ -8,9 +8,15 @@ const app = express();
 app.use(favicon(__dirname + '/icon/favicon.png'));
 
 
-
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, )));
+
+app.get('*', function(req, res){
+    if (req.accepts('html')) {
+        res.send('404', '<script>location.href = "/err404.html";</script>');
+        return;
+    }
+});
 
 
 app.get('/*', function (req, res) {
