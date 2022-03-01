788bfd19a4b8a9e4ca0a218ce14ba68f4ca85e5a
hotmilk0071 <spotam005@gmail.com>
2022-03-01 21:25:58 +0400

    initial

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 3000;
-
-
 const app = express();
+const RewriteOptions = {
+    file: path.resolve(__dirname, '.htaccess'),
+    verbose: (process.env.ENV_NODE === 'development'),
+    watch: (process.env.ENV_NODE === 'development'),
+};
+
 app.use(favicon(__dirname + '/icon/favicon.png'));
+
 
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, )));
+
 
 app.get('*', function(req, res){
     if (req.accepts('html')) {
