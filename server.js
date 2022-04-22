806141df68b0fcd897e17d1aaa4ea4381860c1cb
hotmilk0071 <spotam005@gmail.com>
2022-04-22 11:42:49 +0400

    initial

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
+const RewriteMiddleware = require('express-htaccess-middleware');
+const RewriteOptions = {
+    file: path.resolve(__dirname, '.htaccess'),
+    verbose: (process.env.ENV_NODE === 'development'),
+    watch: (process.env.ENV_NODE === 'development'),
+};
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(favicon(__dirname + '/icon/favicon.png'));
 
-
+app.use(RewriteMiddleware(RewriteOptions));
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, )));
